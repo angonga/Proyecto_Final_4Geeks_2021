fed46ffccad59e185198555679885965f80f033d
angonga <andre.gari.2991@gmail.com>
2021-05-05 02:38:42 +0000

    Changes Andre

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -4,6 +4,8 @@ import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
+import Register from "./pages/register";
+import Login from "./pages/login";
 import { Single } from "./pages/single";
 import injectContext from "./store/appContext";
 
@@ -27,6 +29,12 @@ const Layout = () => {
 						</Route>
 						<Route exact path="/demo">
 							<Demo />
+						</Route>
+						<Route exact path="/register">
+							<Register />
+						</Route>
+						<Route exact path="/login">
+							<Login />
 						</Route>
 						<Route exact path="/single/:theid">
 							<Single />
